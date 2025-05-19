98e712cfafe5fae4acd48957143c62952254db33
McCyberroy <davidkiiss8@gmail.com>
2025-05-19 05:41:25 +0200

    tweening issue

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,8 +1,25 @@
-extends ProgressBar
+extends Control
 
-@onready var parent = get_parent()
+@onready var Parent     = get_parent()
+@onready var Health     = $Health
+@onready var Alteration = $Alteration
+
+@onready var parent_health_ratio : float = Parent.current_health / Parent.max_health
+@onready var bar_health_ratio    : float = Health.size.x / Health.value
+
+func alteration_position() -> Vector2:
+	var _parent_health_ratio : float = Parent.current_health / Parent.max_health
+	return Vector2(Health.size.x * _parent_health_ratio, Health.position.y + Health.size.y)
 
 func _process(delta):
-	max_value = parent.max_health
-	min_value = 0
-	value     = parent.current_health
+	
+	var tween_health_value = create_tween()
+	var tween_alteration_position = create_tween()
+	Health.max_value = Parent.max_health
+	Health.min_value = 0
+
+	tween_health_value.tween_property(Health    , "value"   , Parent.current_health, 1)
+	tween_alteration_position.tween_property(Alteration, "position", alteration_position(), 1)
+	
+	Alteration.max_value = Parent.current_health - Health.value
+	Alteration.value = Parent.current_health - Health.value
