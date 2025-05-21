196a94c4cc5a30d81f0e9e936e7152e52d16fafb
McCyberroy <davidkiiss8@gmail.com>
2025-05-21 06:23:34 +0200

    finalized

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 @onready var parent: Object = get_parent()
 @onready var parent_health_ratio: float = parent.health / parent.max_health
 
@@ -9,24 +10,45 @@ func _process(delta):
 	positive_alteration()
 	negative_health_change()
 	negative_alteration()
+	update_max_health()
 
 
 func positive_health_change() -> void:
-	if parent.health > $HealthBar.value:
+	if $HealthBar.value < parent.health:
 		$HealthBar.value = lerp($HealthBar.value, parent.health, 0.05)
 
+
 func positive_alteration() -> void:
-	if parent.health > $AlterationBar.value:
-		$AlterationBar.value = lerp($AlterationBar.value, parent.health, 0.1)
-		$AlterationBar.get("theme_override_styles/fill").bg_color = Color8(0, 150, 0)
+	if $AlterationBar.value < parent.health:
+		$AlterationBar.value = lerp($AlterationBar.value, parent.health, 0.5)
 
 
 func negative_health_change() -> void:
 	if parent.health < $HealthBar.value:
-		$HealthBar.value = lerp($HealthBar.value, parent.health, 0.1)
+		$HealthBar.value = lerp($HealthBar.value, parent.health, 0.5)
 
 
 func negative_alteration() -> void:
 	if parent.health < $AlterationBar.value:
 		$AlterationBar.value = lerp($AlterationBar.value, parent.health, 0.05)
+
+
+func alteration_color_switch() -> void:
+	if parent.health < $AlterationBar.value:
 		$AlterationBar.get("theme_override_styles/fill").bg_color = Color8(255, 150, 0)
+	
+	if $AlterationBar.value < parent.health:
+		$AlterationBar.get("theme_override_styles/fill").bg_color = Color8(0, 150, 0)
+
+
+func update_max_health() -> void:
+	$HealthBar.max_value = parent.max_health
+	$AlterationBar.max_value = parent.max_health
+
+
+func debug() -> void:
+	print(
+		"PH[" + str(parent.health) + "]" 
+		+ " HB[" + str($HealthBar.value) + "]"
+		+ " AB[" + str($AlterationBar.value) +"]"
+	)
