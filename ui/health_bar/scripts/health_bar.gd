f48d035b42dc0ee22aa8324c9b6cf40cb02e4de9
McCyberroy <142697008+McCyberroy@users.noreply.github.com>
2025-06-02 04:18:00 +0200

    Updated HealthBar (#16)
    - rewrote sections of code
    - fixed all warnings
    - added separate Background Node (Panel)
    - added separate Frame Node (Panel)
    - renamed Nodes inside health_bar.tscn

--- ui/health_bar/scripts/health_bar.gd
+++ ui/health_bar/scripts/health_bar.gd
@@ -1,6 +1,6 @@
 extends PanelContainer
 
-
+#region export
 @export_group("Health Bar")
 ## The instance this [color=light_green]Health Bar[/color] 
 ## is related to.[br]
@@ -18,27 +18,25 @@ extends PanelContainer
 @export_subgroup("Animation")
 ## The time (in seconds) the [color=light_green]Health Bar[/color]
 ## will take to animate changes in [color=hot_pink]health[/color].
-@export_range(0, 1, 0.05) var animation_duration: float = 0.4
+@export_range(0, 1, 0.1) var animation_duration: float = 0.4
 ## The shake duration (in seconds) after damage was received.
-@export_range(0, 1, 0.05) var shake_duration: float = 0.25
+@export_range(0, 1, 0.1) var shake_duration: float = 0.2
 ## The strength of the shake.
-@export_range(0, 10, 0.05) var shake_strength: float = 4
+@export_range(0, 10, 0.1) var shake_strength: float = 4
 
+#endregion
+
+#region variables
 
 var agent_health_copy: int : set = _on_agent_health_changed
 var agent_health_ratio: float
-var health_bar_tween: Tween = create_tween()
-var alteration_bar_tween: Tween = create_tween()
 
+#endregion
 
-
-
-
-
-
+#region gameloop
 
 func _ready():
-	if agent is not Node:
+	if agent == null:
 		agent = get_parent()
 	update_properties()
 
@@ -47,17 +45,17 @@ func _process(_delta):
 	update_properties()
 	handle_shake()
 	handle_flash()
+	debug_values(false)
 
 func _on_agent_health_changed(new_health):
 	if agent_is("damaged"):
-		health_bar("tween", "", animation_duration)
+		health_bar("tween", "value", animation_duration)
 		$AlterationTweenTimer.start(animation_duration)
 		alteration_bar("set", "color", damage_color)
 		$ShakeTimer.start(shake_duration)
 	
 	elif agent_is("healed"):
-		#tween_alteration_bar(animation_duration)
-		alteration_bar("tween", "", animation_duration)
+		alteration_bar("tween", "value", animation_duration)
 		$HealthTweenTimer.start(animation_duration)
 		alteration_bar("set", "color", heal_color)
 	
@@ -65,52 +63,54 @@ func _on_agent_health_changed(new_health):
 
 
 func _on_alteration_tween_timer_timeout():
-	alteration_bar("tween", "", animation_duration)
+	alteration_bar("tween", "value", animation_duration)
 
 
 func _on_health_tween_timer_timeout():
-	health_bar("tween", "", animation_duration)
+	health_bar("tween", "value", animation_duration)
 
+#endregion
 
-
-
-
-
-
+#region functions
 
 func update_properties() -> void:
 	agent_health_copy = agent.health
 	agent_health_ratio = agent.health / agent.max_health
-	$HealthBar.max_value = agent.max_health
-	$AlterationBar.max_value = agent.max_health
+	$Health.max_value = agent.max_health
+	$Alteration.max_value = agent.max_health
 
 
 func handle_shake() -> void:
 	if not $ShakeTimer.is_stopped():
-		shake()
+		shake_effect(true)
 	else:
-		reposition_after_shake()
+		shake_effect(false)
 
 
-func shake() -> void:
-		var random_x: float = randf_range(position.x - shake_strength, position.x + shake_strength)
-		var random_y: float = randf_range(position.y - shake_strength, position.y + shake_strength)
-		var random_position := Vector2(random_x, random_y).round()
-		$HealthBar.position = random_position
-		$AlterationBar.position = random_position
+func shake_effect(shake: bool) -> void:
+	if shake:
+		var random_x: float = randf_range(-shake_strength, shake_strength)
+		var random_y: float = randf_range(-shake_strength, shake_strength)
+		var random_position: Vector2 = Vector2(random_x, random_y).round()
+		$Background.position = random_position
+		$Alteration.position = random_position
+		$Health.position = random_position
+		$Frame.position = random_position
 		$Shading.position = random_position
-
-
-func reposition_after_shake() -> void:
-	$HealthBar.position = position
-	$AlterationBar.position = position
-	$Shading.position = position
+	
+	else:
+		$Background.position = Vector2.ZERO
+		$Alteration.position = Vector2.ZERO
+		$Health.position = Vector2.ZERO
+		$Shading.position = Vector2.ZERO
+		$Frame.position = Vector2.ZERO
 
 
 func handle_flash() -> void:
 	if agent_is("low health") and health_bar("get", "alpha") > 0.0:
 		health_bar("subtract", "alpha", 0.1)
 		alteration_bar("subtract", "alpha", 0.1)
+	
 	else:
 		health_bar("set", "alpha", 1.0)
 		alteration_bar("set", "alpha", 1.0)
@@ -119,79 +119,75 @@ func handle_flash() -> void:
 func agent_is(question: String = ""):
 	if question == "damaged":
 		return agent.health < agent_health_copy
+	
 	elif question == "healed":
 		return agent.health > agent_health_copy
+	
 	elif question == "low health":
 		return agent_health_ratio < 0.25
+	
 	else:
 		return false
 
 
-func tween_health_bar(duration: float) -> void:
-	health_bar_tween.kill()
-	health_bar_tween = create_tween()
-	health_bar_tween.tween_property($HealthBar, "value", agent.health, duration)
-
-
-func tween_alteration_bar(duration: float) -> void:
-	alteration_bar_tween.kill()
-	alteration_bar_tween = create_tween()
-	alteration_bar_tween.tween_property($AlterationBar, "value", agent.health, duration)
-
-
 func alteration_bar(method: String = "", property: String = "", value: Variant = null):
 	if method == "set" and property == "color":
-		$AlterationBar.get("theme_override_styles/fill").bg_color = value
-		
+		$Alteration.get("theme_override_styles/fill").bg_color = value
+	
 	elif method == "set" and property == "alpha":
-		$AlterationBar.get("theme_override_styles/fill").bg_color.a = value
+		$Alteration.get("theme_override_styles/fill").bg_color.a = value
 	
-	if method == "get" and property == "color":
-		return $AlterationBar.get("theme_override_styles/fill").bg_color
-		
+	elif method == "get" and property == "color":
+		return $Alteration.get("theme_override_styles/fill").bg_color
+	
 	elif method == "get" and property == "alpha":
-		return $AlterationBar.get("theme_override_styles/fill").bg_color.a
+		return $Alteration.get("theme_override_styles/fill").bg_color.a
 	
-	if method == "add" and property == "alpha":
-		$AlterationBar.get("theme_override_styles/fill").bg_color.a += value
+	elif method == "add" and property == "alpha":
+		$Alteration.get("theme_override_styles/fill").bg_color.a += value
 	
-	if method == "subtract" and property == "alpha":
-		$AlterationBar.get("theme_override_styles/fill").bg_color.a -= value
+	elif method == "subtract" and property == "alpha":
+		$Alteration.get("theme_override_styles/fill").bg_color.a -= value
 	
-	if method == "tween":
-		alteration_bar_tween.kill()
-		alteration_bar_tween = create_tween()
-		alteration_bar_tween.tween_property($AlterationBar, "value", agent.health, value)
+	elif method == "tween":
+		create_tween().tween_property($Alteration, property, agent.health, value)
+	
+	else:
+		push_error("wrong count or wrong type of argument supplied")
 
 
 func health_bar(method: String = "", property: String = "", value: Variant = null):
 	if method == "set" and property == "color":
-		$HealthBar.get("theme_override_styles/fill").bg_color = value
-		
+		$Health.get("theme_override_styles/fill").bg_color = value
+	
 	elif method == "set" and property == "alpha":
-		$HealthBar.get("theme_override_styles/fill").bg_color.a = value
+		$Health.get("theme_override_styles/fill").bg_color.a = value
 	
-	if method == "get" and property == "color":
-		return $HealthBar.get("theme_override_styles/fill").bg_color
-		
+	elif method == "get" and property == "color":
+		return $Health.get("theme_override_styles/fill").bg_color
+	
 	elif method == "get" and property == "alpha":
-		return $HealthBar.get("theme_override_styles/fill").bg_color.a
+		return $Health.get("theme_override_styles/fill").bg_color.a
 	
-	if method == "add" and property == "alpha":
-		$HealthBar.get("theme_override_styles/fill").bg_color.a += value
+	elif method == "add" and property == "alpha":
+		$Health.get("theme_override_styles/fill").bg_color.a += value
 	
-	if method == "subtract" and property == "alpha":
-		$HealthBar.get("theme_override_styles/fill").bg_color.a -= value
+	elif method == "subtract" and property == "alpha":
+		$Health.get("theme_override_styles/fill").bg_color.a -= value
 	
-	if method == "tween":
-		alteration_bar_tween.kill()
-		alteration_bar_tween = create_tween()
-		alteration_bar_tween.tween_property($HealthBar, "value", agent.health, value)
+	elif method == "tween":
+		create_tween().tween_property($Health, property, agent.health, value)
+	
+	else:
+		push_error("wrong count or wrong type of argument supplied")
 
 
-func debug_values() -> void:
-	print(
-		"PH[" + str(agent.health) + "]" 
-		+ " HB[" + str($HealthBar.value) + "]"
-		+ " AB[" + str($AlterationBar.value) +"]"
-	)
+func debug_values(debug: bool) -> void:
+	if debug:
+		print(
+			"agent_health: " + str(agent.health) + "    "
+			+ "bar_health: " + str($Health.value) + "    "
+			+ "bar_alteration: " + str($Alteration.value)
+		)
+
+#endregion
